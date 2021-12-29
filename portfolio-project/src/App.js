9341f89674d255416b57792cfbd8354f92ab725b
sohailfarooqi714 <sohailfarooqi714@gmail.com>
2021-12-29 15:44:24 +0500

    12/29/2021 Updated
    Updated

--- portfolio-project/src/App.js
+++ portfolio-project/src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
+import Particles from "react-tsparticles";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 
 function App() {
   return (
     <>
-    <Navbar/>
-    <Header/>
+    <Navbar />
+    <Header />
     </>
   );
 }
