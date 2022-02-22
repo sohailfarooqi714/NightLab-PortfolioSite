d775792f8b4a429dd8327869821c2357c61c30fe
sohailfarooqi714 <sohailfarooqi714@gmail.com>
2022-02-22 11:30:27 +0500

    Updates 02/22/2022
    1.) Content Creation Carousel.
    2.) Content creation button linked to drive.
    3.) Portfolio Carousel background color applied.

--- portfolio-project/src/components/PortfolioCarousel3.js
+++ portfolio-project/src/components/PortfolioCarousel3.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 // Import Project Images
-import Khattaapp1 from "../Khattaapp1.png";
-import Khattaapp2 from "../Khattaapp2.png";
-import Khattaapp3 from "../Khattaapp3.png";
-import Dastgyr4 from "../Dastgyr4.png";
-import Dastgyr5 from "../USER-JOURNEY.png";
+import Content1 from "../Content1.png";
+import Content2 from "../Content2.png";
+import Content3 from "../Content3.png";
+import Content4 from "../Content4.png";
+import Content5 from "../Content5.png";
 
 
 const PortfolioCarousel3 = () => {
@@ -19,13 +19,19 @@ const PortfolioCarousel3 = () => {
             transitionTime={750}
             interval={3000}>
             <>
-                <img className="carousel1-image" src={ Khattaapp1 }alt = "Image 1" />
+                <img className="carousel-image" src={ Content1 }alt = "Image 1" />
             </>
             <>
-                <img className="carousel1-image" src={ Khattaapp2 }alt = "Image 2" />
+                <img className="carousel-image" src={ Content2 }alt = "Image 2" />
             </>
             <>
-                <img className="carousel1-image" src={ Khattaapp3 }alt = "Image 3" />
+                <img className="carousel-image" src={ Content3 }alt = "Image 3" />
+            </>
+            <>
+                <img className="carousel-image" src={ Content4 }alt = "Image 4" />
+            </>
+            <>
+                <img className="carousel-image" src={ Content5 }alt = "Image 5" />
             </>
          </Carousel>;
 };
